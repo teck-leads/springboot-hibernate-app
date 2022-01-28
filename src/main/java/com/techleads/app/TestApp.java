0735110debc3da70106cc44f5019999d9cba2b07
techleads <anupoju.anand@gmail.com>
2022-01-28 19:45:21 +0530

    settings.put(Environment.HBM2DDL_AUTO, update);

--- src/main/java/com/techleads/app/TestApp.java
+++ src/main/java/com/techleads/app/TestApp.java
@@ -9,7 +9,7 @@ public class TestApp {
 
 	public static void main(String[] args) {
 		StudentRepository studentDao = new StudentRepository();
-	        Student student = new Student(101,"madhav", "anupoju", "madhav@tm.com");
+	        Student student = new Student(102,"dill", "anupoju", "madhav@tm.com");
 	        studentDao.saveStudent(student);
 
 	        List < Student > students = studentDao.getStudents();
--- src/main/java/com/techleads/app/util/HibernateUtil.java
+++ src/main/java/com/techleads/app/util/HibernateUtil.java
@@ -28,7 +28,7 @@ public class HibernateUtil {
 
                 settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
 
-                settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+                settings.put(Environment.HBM2DDL_AUTO, "update");
 
                 configuration.setProperties(settings);
 
